69e167bbdd57ac08ba00a02f110e24f43ec0d0aa
DESKTOP-39CBMEP\User <nara20210617@gmail.com>
2023-07-06 13:49:55 +0900

    feat: 싱글톤 패턴 구현

---
+++ src/Chapter6/singleton/CompanyTest.java
@@ -0,0 +1,18 @@
+package Chapter6.singleton;
+
+import java.util.Calendar;
+
+public class CompanyTest {
+    public static void main(String[] args) {
+        Company c1 = Company.getInstance();
+        Company c2 = Company.getInstance();
+        Company c3 = Company.getInstance();
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3 == c2);
+        // Company c4 = new Company(); // error
+
+        Calendar cal = Calendar.getInstance();
+    }
+}
