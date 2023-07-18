7908b15dd5a2743770ba79e3c0e433be8719a7c5
DESKTOP-39CBMEP\User <nara20210617@gmail.com>
2023-07-18 11:35:17 +0900

    feat: 익명 이너클래스 제작 완료

--- src/chapter13/Innerclass/LocalInnerTest.java
+++ src/chapter13/Innerclass/LocalInnerTest.java
@@ -3,23 +3,28 @@ package chapter13.Innerclass;
 class Outer{
     int outNum = 100;
     static int sNum = 200;
+
+    //익명 이너클래스 - 단 하나의 추상클래스나 단 하나의 인터페이스만 구현할 수 있다
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            System.out.println("runnable");
+        }
+    };
     public Runnable getRunnable(int i) {
         int localNum = 100;
-        class MyRunnable implements Runnable{
-
+        return new Runnable() {
             @Override
             public void run() {
-                //localNum += 100;
-                //i += 10;
 
                 System.out.println(sNum);
                 System.out.println(outNum);
                 System.out.println(localNum);
                 System.out.println(i);
             }
-        }
-        return new MyRunnable();
-    }
+        };
+
+   }
 }
 public class LocalInnerTest {
     public static void main(String[] args) {
@@ -28,5 +33,7 @@ public class LocalInnerTest {
         //outer.getRunnable(0).run();
         Runnable runnable = outer.getRunnable(10);
         runnable.run();
+
+        outer.runnable.run();
     }
 }
